562462d88e1dc71c599b94374afcb9e1188487b5
김태현 <rlaougs5@gmail.com>
2022-03-12 01:38:55 +0900

    [Programmers] 문자열을 정수로 바꾸기

---
+++ src/com/company/Integer_parseInt1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class Integer_parseInt1 {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+    public static void main(String[] args) {
+        Integer_parseInt1 se = new Integer_parseInt1();
+        System.out.println(se.solution("33"));
+    }
+
+}
+
