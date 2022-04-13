645c5ea5169a1427ade206d04363627890e7504c
zsoon96 <pres0616@gmail.com>
2022-04-13 09:15:25 +0900

    1019~1022(기초-입출력)

--- src/main/java/com/hanghae99/codeup100/CodeUp100Application.java
+++ src/main/java/com/hanghae99/codeup100/CodeUp100Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 @SpringBootApplication
 public class CodeUp100Application {
@@ -75,30 +76,94 @@ public class CodeUp100Application {
         //22.04.12=====================================================================================
 
         // 1014 - 문자 2개 입력받아 순서 바꿔 출력
-        char ch1 = sc.next().charAt(0), ch2 = sc.next().charAt(1);
-        System.out.println(ch2 +", "+ ch1);
+//        char ch1 = sc.next().charAt(0), ch2 = sc.next().charAt(1);
+//        System.out.println(ch2 +", "+ ch1);
 
         // 1015 - 실수 입력받아 둘째 자리까지 출력
         // 다시 float값으로 바꿔주려면 Float.parseFloat() 메소드로 변경 가능
-        String result;
-        float f = sc.nextFloat();
-        result = String.format("%.2f",f);
-        System.out.println(result);
+//        String result;
+//        float f = sc.nextFloat();
+//        result = String.format("%.2f",f);
+//        System.out.println(result);
 
         // 1017 - 정수 1개 입력받아 3번 출력하기
-        int i = sc.nextInt();
-        System.out.println(i+", "+i+", "+i);
+//        int i = sc.nextInt();
+//        System.out.println(i+", "+i+", "+i);
 
         // 1018 - 시간 입력받아 그대로 출력하기
         // next(): 사이띄기, tab키, enter키를 구분문자로 분리해서 분리된 자료만 읽어간다.
-        String time = sc.next();
-        System.out.printf("%s",time);
+//        String time = sc.next();
+//        System.out.printf("%s",time);
         // 또는 nextLine() 메소드 사용
         // nextLine(): 한 줄 단위로 입력한다.
-        sc.nextLine(); // 없으면 아래 구문이 실행없이 종료되버림... = 입력버퍼를 비워주는 역할을 한다.
+//        sc.nextLine(); // 없으면 아래 구문이 실행없이 종료되버림... = 입력버퍼를 비워주는 역할을 한다.
         // 입력버퍼: 다른 장치에서 데이터를 수신할 때 일어나는 시간의 차이나 데이터의 흐름 속도의 차이를 보상하기 위해서 사용하는 일시적인 저장 장치.
         // -> 전에 저장된 데이터가 입력 버퍼에 남아있어서 별도의 입력을 안했는데도 넘어가버리는 것.
-        String time2 = sc.nextLine();
-        System.out.println(time2);
+//        String time2 = sc.nextLine();
+//        System.out.println(time2);
+
+        // 1019 -연월일 입력받아 순서 바꿔 출력하기
+        // 먼저 입력된 문자열을 .기준으로 자른 후, 문자열 리스트에 넣어주기
+        // split을 사용할 땐 앞에 백슬래시 2개 붙여줘야 함
+        String[] ymd = sc.nextLine().split("\\.");
+        // integer.parseInt로 정수형으로 변환 후 저장
+        int y = Integer.parseInt(ymd[0]);
+        int m = Integer.parseInt(ymd[1]);
+        int d = Integer.parseInt(ymd[2]);
+        // 출력값이 dd-mm-yyyy 되어야하는데, 한 자리 수로 입력할 경우 빈공간에는 0을 넣어서 출력하기.
+        System.out.printf(String.format("%02d.%02d.%04d",d,m,y));
+
+        // 또는 문자열로 출력하는 경우
+        String ymd2 = sc.nextLine();
+        // StringTokenizer: 구분자를 이용해 문자열 쪼개주기 > (파싱할 문자열, 구분자)를 인자로 받는다.
+        StringTokenizer split = new StringTokenizer(ymd2,".");
+        // 정수형의 리스트 생성
+        int [] ymd3 = new int[3];
+        // index 값 초기화
+        int i = 0;
+        // split에 hasMoreTokens메서드를 통해 StringTokenizer에 사용할 수 있는 토큰이 더 있는지 확인
+        // 참이면 go, 거짓이면 stop
+        while (split.hasMoreTokens()) {
+            // 만들어준 정수형 리스트에 해당 문자열을 정수형으로 변환하여 담아줄건데,
+            // nextToken() 메서드를 통해 다음 토큰을 불러와 리스트에 i만큼씩 담아주기
+            ymd3[i] = Integer.parseInt(split.nextToken());
+            // 1씩 증가
+            i += 1;
+        }
+        // String.format을 사용해 문자열 형식을 정수 형식으로 바꿔서 각각 변수에 담아주기
+        // 정해진 출력값의 길이 설정 및 한자리 수를 입력할 경우 빈 공간에는 0을 넣어서 출력
+        String yy = String.format("%04d", ymd3[0]);
+        String mm = String.format("%02d", ymd3[1]);
+        String dd = String.format("%02d", ymd3[2]);
+        System.out.println(dd+"-"+mm+"-"+yy);
+
+
+        // 1020 - 주민번호 입력받아 형태 바꿔 출력하기
+        String num = sc.nextLine();
+        // substring으로 '-'기준 앞/뒤로 나눠주기
+        String front = num.substring(0,6);
+        String back = num.substring(7);
+        System.out.println(front+back);
+        // 또는 split 메소드 이용
+        String num2 = sc.nextLine();
+        String[] divider = num2.split("\\-");
+        // String.valueOf를 통해 인자(어떠한 형태이든)를 문자열 객체로 형변환 > 근데 굳이 여기서 쓸 필요가?
+        String front2 = String.valueOf(divider[0]); // divider[0]로도 가능
+        String back2 = String.valueOf(divider[1]); // divider[1]로도 가능
+        System.out.println(front2+back2);
+        // 또는 replace 메소드 이용
+        // replace: 문자열 치환 > 변경하고 싶은 문자열을 찾아 치환(찾을 문자열, 바꿀 문자열)
+        String num3 = sc.nextLine();
+        System.out.println(num3.replace("-",""));
+
+        // 1021 - 단어 1개 입력받아 그대로 출력하기
+        String word = sc.next();
+        System.out.println(word);
+
+        // 입력 버퍼 제거
+        sc.nextLine();
+        // 1022 - 문장 1개 입력받아 그대로 출력하기
+        String sentence = sc.nextLine();
+        System.out.println(sentence);
     }
 }
